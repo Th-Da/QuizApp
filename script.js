1368ff5c094643d01368fe972a00e9b3dc9e473c
tdNoobie <thomas.danielse@gmx.de>
2022-05-21 14:54:40 +0200

    add progressbar + add sounds

--- script.js
+++ script.js
@@ -50,25 +50,35 @@ let questions = [
 ];
 
 let currentQuestion = 0;
+let correctAnswers = 0;
+let AUDIO_SUCCESS = new Audio('audio/correctAnswer.mp3')
+let AUDIO_FAIL = new Audio('audio/wrongAnswer.mp3')
+let AUDIO_END = new Audio('audio/winSound.mp3')
+
 
 function init() {
     currentQuestion = 0;
+    correctAnswers = 0;
     let allQuestions = document.getElementById('allQuestions').innerHTML = questions.length;;
     showQuestion();
 }
 
 function showQuestion() {
-    if (currentQuestion >= questions.length) {
-        endOfQuizz();
+    if (gameIsOver()) {
+        endOfQuiz();
 
     } else {
+        updateProgress();
         renderNextQuestion();
     }
 }
 
+function gameIsOver() {
+    return currentQuestion >= questions.length;
+}
+
 function renderNextQuestion() {
     let current = questions[currentQuestion];
-    document.getElementById('currentQuestion').innerHTML = currentQuestion + 1;
     document.getElementById('endscreen').style = 'display: none';
     document.getElementById('cardRight').style = ``;
     document.getElementById('questionText').innerHTML = current['question'];
@@ -78,10 +88,20 @@ function renderNextQuestion() {
     document.getElementById('answer4').innerHTML = current['answer4'];
 }
 
-function endOfQuizz() {
+function updateProgress() {
+    let percent = currentQuestion / questions.length * 100;
+    document.getElementById('progressBar').innerHTML = percent.toFixed(0) + `%`;
+    document.getElementById('progressBar').style.width = percent + `%`;
+    document.getElementById('currentQuestion').innerHTML = currentQuestion + 1;
+}
+
+function endOfQuiz() {
     document.getElementById('cardRight').style = 'display: none';
     document.getElementById('endscreen').style = ``;
+    document.getElementById('allQuestionsEndscreen').innerHTML = questions.length;
+    document.getElementById('correctAnswers').innerHTML = correctAnswers;
     document.getElementById('cardLeft').innerHTML += generateButtonHTML();
+    AUDIO_END.play();
 }
 
 function answer(answer) {
@@ -91,9 +111,12 @@ function answer(answer) {
     if (selectedAnswer == rightAnswer) {
         console.log('right');
         document.getElementById(answer).parentNode.classList.add('bg-success');
+        correctAnswers ++;
+        AUDIO_SUCCESS.play();
     } else {
         document.getElementById(answer).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
     }
     document.getElementById('buttonNext').disabled = false;
 }
